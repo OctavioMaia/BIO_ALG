773c121fa87dad490be4177ba108d8f365faad81
Octávio Maia <a71369@alunos.uminho.pt>
2017-09-22 16:17:11 +0100

    teste python

---
+++ teste.py
@@ -0,0 +1,28 @@
+import math
+
+print("teste")
+x=4
+y=5
+print(x+y)
+
+print(math.e)
+
+z=[20,30,40]
+print(x+z[2])
+
+z.append(9)
+print(z)
+
+z.reverse()
+print(z)
+
+print(max(z))
+
+str1="ola"
+str2='ola'
+print("aspas:"+str1)
+print("plicas:"+ str2)
+
+dict={'A':'Ala','C':'Cys','S':'Ser'}
+print(dict['C'])
+print(dict['G'])
